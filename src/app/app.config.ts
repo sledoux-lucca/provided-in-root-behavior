7fa02354ac2fbe0e3c6901aae48ca494997ce096
Sylvain Ledoux <sledoux@luccasoftware.com>
2025-02-25 11:21:40 +0100

    feat: app-initializer service

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,17 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  inject,
+  provideAppInitializer,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { AppInitService } from './services/app-init.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    // {
-    //   provide: APP_INITIALIZER,
-    //   useValue: () => inject(AppInitService).init(),
-    //   multi: true,
-    // },
+    provideAppInitializer(() => inject(AppInitService).init()),
   ],
 };
