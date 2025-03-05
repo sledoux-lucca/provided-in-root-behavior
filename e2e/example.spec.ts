ebc2825129b387fc6db964a74a22b6371abb21d8
Sylvain Ledoux <sledoux@luccasoftware.com>
2025-03-05 19:25:30 +0100

    feat: update basic e2e test

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,7 +1,19 @@
 import { expect, test } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('/');
+test.describe('Basic navigation', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/');
+  });
 
-  await expect(page).toHaveTitle('ProvidedInRootBehavior');
+  test('has title', async ({ page }) => {
+    await expect(page).toHaveTitle('ProvidedInRootBehavior');
+  });
+
+  test('get started link', async ({ page }) => {
+    await page.getByRole('link', { name: 'Child 1' }).click();
+
+    await expect(
+      page.getByRole('heading', { name: 'Child1Component' })
+    ).toBeVisible();
+  });
 });
